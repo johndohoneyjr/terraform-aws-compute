19155608215054d50fc7c91a1ac813f115f8feed
John Dohoney Jr <johndohoneyjr@Blackhawk.local>
2019-04-03 13:54:46 -0700

    Updated Vars

--- variables.tf
+++ variables.tf
@@ -4,17 +4,27 @@ variable "key_name" {
 
 variable "subnet_ips" {
     type = "list"
+    default=["10.0.20.0", "10.0.30.0"]
 }
 variable "instance_count" {
   default=2
 }
-variable "instance_type" {}
+variable "instance_type" {
+  default="t2.medium"
+}
 
-variable "security_group" {}
+variable "security_group" {
+  default="default"
+}
 
 variable "subnets" {
  type = "list"
+ default=["10.0.20.0", "10.0.30.0"]
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  default="none"
+}
+variable "aws_secret_key" {
+  default="none"
+}
